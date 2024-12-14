e2612de1466fd0148e34c4c5ad397ec283849a3f
JustinTuano <Justin.tuano.college@ustangelicum.edu.ph>
2024-12-14 14:03:10 +0800

    hello

---
+++ BMI/main.py
@@ -0,0 +1,5 @@
+weight = int(input("Enter the weight: "))
+height = int(input("Enter the height: "))
+height**=2
+bmi=weight/height
+print(f"This is your BMI {bmi}")
---
+++ TEMPERATURE CONVERTER/main.py
@@ -0,0 +1,5 @@
+celcius=float(input("Enter value in celcius: "))
+print(f"{celcius} celcius is equivalent to {(1.8 * celcius) + 32}farenheit")
+
+farenheit = float(input("Enter value in farenheit"))
+print (f"{farenheit} farenheit is equivalent to {0.56 * (farenheit - 32)} celcius") 
